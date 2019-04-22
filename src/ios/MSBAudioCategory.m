de541e58bd1fc4804df7f3b6e47895e94e7ef84b
Tom Sullivan <tom@msbit.com.au>
2019-04-22 19:04:22 +1000

    Error out on invalid `AVAudioSessionCategory`
    Check whether the provided category key maps to a known
    `AVAudioSessionCategory` and cause a promise rejection if not
    found.

--- src/ios/MSBAudioCategory.m
+++ src/ios/MSBAudioCategory.m
@@ -34,19 +34,26 @@ NSDictionary *audioCategories;
     NSString *audioCategory = (NSString *)command.arguments[0];
     NSString *enumAudioCategory = [audioCategories objectForKey:audioCategory];
 
-    NSError *error = nil;
-    BOOL success =
-        [[AVAudioSession sharedInstance] setCategory:enumAudioCategory
-                                               error:&error];
-
     CDVPluginResult *result;
 
-    if (success) {
-      result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                 messageAsString:@"Success"];
+    if (enumAudioCategory == nil) {
+      result =
+          [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                            messageAsString:@"Invalid AVAudioSessionCategory"];
     } else {
-      result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                 messageAsString:error.description];
+
+      NSError *error = nil;
+      BOOL success =
+          [[AVAudioSession sharedInstance] setCategory:enumAudioCategory
+                                                 error:&error];
+
+      if (success) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                   messageAsString:@"Success"];
+      } else {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                   messageAsString:error.description];
+      }
     }
 
     [self.commandDelegate sendPluginResult:result
